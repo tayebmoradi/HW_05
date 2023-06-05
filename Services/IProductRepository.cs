bab50ed2593d3496a10a09a0395b16f7477fcc0c
tayebmoradi <tayeb.mora69@gmail.com>
2023-06-05 16:43:10 +0330

    Add Services and Utility

---
+++ Services/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.ViewModel;
+
+namespace Services
+{
+    public interface IProductRepository
+    {
+        string AddProduct(ProductDTO product);
+        List<Product> GetProductList();
+        string GetProductById(int id);
+    }
+}
